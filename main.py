304d0759d2a87c6d7b63794d74beb16067b71426
SC-V <37342773+SC-V@users.noreply.github.com>
2023-07-12 14:29:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -145,10 +145,9 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
                 report_courier_park = "No courier yet"
             try:
                 report_return_reason = str(claim['route_points'][1]['return_reasons'])
-                report_return_comment = claim['route_points'][1]['return_comment']
             except:
                 report_return_reason = "No return reasons"
-                report_return_comment = "No return comments"
+            report_return_comment = "Return comments are disabled"
             try:
                 report_route_id = claim['route_id']
             except:
